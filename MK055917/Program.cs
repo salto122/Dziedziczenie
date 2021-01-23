39a5066a97a33c8af1708bc77598ad032012e168
mikeUwU <salto122@outlook.com>
2021-01-23 00:42:48 -0800

    test

---
+++ MK055917/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MK055917
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
